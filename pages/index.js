7958fa653fcdf646e70753c4906efeb6889b9957
Alexander Morugin <nobilis@bk.ru>
2022-10-19 19:55:13 +0300

    friends 3

---
+++ pages/index.js
@@ -0,0 +1,80 @@
+//@ts-check
+
+// Переменные
+// Mobile - Модальное окно "Меню 1 ур"
+const buttonHeaderMenu = document.querySelector('.header__button-menu');
+const popup = document.querySelector('.popup');
+const headerMenuMobile = document.querySelector('.header_menu_mobile');
+const buttonHeaderMenuClose = document.querySelector('.popup__button-close');
+
+// Mobile - Модальное окно "Меню Фонд 2.1 ур"
+const linkFund = document.querySelector('.fund');
+const headerFundMobile = document.querySelector('.header_fund_mobile');
+const buttonBackFund = document.querySelector('.back_fund');
+
+// Mobile - Модальное окно "Меню Программы 2.2 ур"
+const linkPrograms = document.querySelector('.programs');
+const headerProgramsMobile = document.querySelector('.header_programs_mobile');
+const buttonBackPrograms = document.querySelector('.back_programs');
+
+
+// Desktop - Модальное окно "Меню Фонд"
+const menuFund = document.querySelector('#menu-fund');
+const headerFundDesktop = document.querySelector('.desktop-menu-fund');
+
+// Desktop - Модальное окно "Меню Программы"
+const menuPrograms = document.querySelector('#menu-programs');
+const headerProgramsDesktop = document.querySelector('.desktop-menu-programs');
+
+
+// Функции Универсальные
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+};
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+};
+
+function togglePopup(popup) {
+  popup.classList.toggle('popup_opened');
+};
+
+
+// Слушатели событий
+// Mobile - Модальное окно "Меню 1 ур"
+buttonHeaderMenu.addEventListener('click', () => {
+  openPopup(headerMenuMobile);
+});
+
+buttonHeaderMenuClose.addEventListener('click', () => {
+  closePopup(headerMenuMobile);
+});
+
+// Mobile - Модальное окно "Меню Фонд 2.1 ур"
+linkFund.addEventListener('click', () => {
+  openPopup(headerFundMobile);
+});
+
+buttonBackFund.addEventListener('click', () => {
+  closePopup(headerFundMobile);
+});
+
+// Mobile - Модальное окно "Меню Программы 2.2 ур"
+linkPrograms.addEventListener('click', () => {
+  openPopup(headerProgramsMobile);
+});
+
+buttonBackPrograms.addEventListener('click', () => {
+  closePopup(headerProgramsMobile);
+});
+
+// Desktop - Модальное окно "Меню Фонд"
+menuFund.addEventListener('click', () => {
+  togglePopup(headerFundDesktop);
+});
+
+// Desktop - Модальное окно "Меню Программы"
+menuPrograms.addEventListener('click', () => {
+  togglePopup(headerProgramsDesktop);
+});
